ffcff8e6916a54cf66382018feda3fa867bae0db
jmatta697 <jmatta1980@hotmail.com>
2018-11-26 15:50:17 -0500

    revisions 1

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def main():
                   0x31, 0x31, 0x98, 0xa2,
                   0xe0, 0x37, 0x07, 0x34]
     state_matrix = arrange_plain_text_into_state_matrix(plain_text)
-    print(state_matrix)
+    print("state matrix\n" + str(np.asarray(state_matrix)) + "\n")
     key = [0x2b, 0x7e, 0x15, 0x16,
            0x28, 0xae, 0xd2, 0xa6,
            0xab, 0xf7, 0x15, 0x88,
            0x09, 0xcf, 0x4f, 0x3c]
     key_matrix = arrange_key_into_matrix(key)
-    print(key_matrix)
+    print("key matrix\n" + str(np.asarray(key_matrix)) + "\n")
 
     added_key = add_key(state_matrix, key_matrix)
-    print(added_key)
+    print("added key matrix\n" + str(np.asarray(added_key)) + "\n")
 
-    Sbox = [[0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76],
+    sbox = [[0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76],
             [0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0],
             [0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15],
             [0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75],
@@ -36,14 +36,31 @@ def main():
             [0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF],
             [0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]]
 
-    byte_sub_array = byte_substitution(added_key, Sbox)
-    print(byte_sub_array)
+    byte_sub_array = byte_substitution(added_key, sbox)
+    print("byte-sub matrix\n" + str(byte_sub_array) + "\n")
 
     row_shifted = shift_row_transformation(byte_sub_array)
-    print(row_shifted)
+    print("row shifted matrix\n" + str(row_shifted) + "\n")
 
     mix_columns_matrix = mix_columns_transformation(row_shifted)
-    print(mix_columns_matrix)
+    print("mix columns matrix\n" + str(mix_columns_matrix) + "\n")
+
+    # special test key - from notes
+    # special_key = [0x2b, 0x7e, 0x15, 0x16,
+    #                0x28, 0xae, 0xde, 0xa6,
+    #                0xab, 0xf7, 0x15, 0x88,
+    #                0x09, 0xcf, 0x4f, 0x3c]
+
+    trans_key_to_columns = arrange_key_into_columns(key)
+    print("key matrix arranged in columns\n" + str(np.asarray(trans_key_to_columns)) + "\n")
+
+    trans_key_to_rows = key_columns_to_rows(trans_key_to_columns)
+    print("key matrix arranged in rows\n" + str(np.asarray(trans_key_to_rows)) + "\n")
+
+    master_key_all_rounds = generate_all_key_columns(trans_key_to_columns, sbox)
+    print("master key for all rounds\n" + str(np.asarray(master_key_all_rounds)) + "\n")
+
+# -----------------------------------------------------------------------------------------------
 
 
 def arrange_plain_text_into_state_matrix(p_text):
@@ -90,22 +107,105 @@ def shift_row_transformation(byt_sub_matx):
 
 def mix_columns_transformation(row_shft_mtrx):
     mix_col_matrx = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    fixed_matrix = [[2, 3, 1, 1],
-                    [1, 2, 3, 1],
-                    [1, 1, 2, 3],
-                    [3, 1, 1, 2]]
+    fixed_matrix = [[0x02, 0x03, 0x01, 0x01],
+                    [0x01, 0x02, 0x03, 0x01],
+                    [0x01, 0x01, 0x02, 0x03],
+                    [0x03, 0x01, 0x01, 0x02]]
     for i in range(len(fixed_matrix)):
         for j in range(len(row_shft_mtrx[0])):
+            mixed_entry = 0
             for k in range(len(row_shft_mtrx)):
-                raw_product = fixed_matrix[i][k] * row_shft_mtrx[k][j]
-                if raw_product > 0xff:
-                    product = raw_product ^ 0b100011011
+                if fixed_matrix[i][k] == 0x02:
+                    raw_product = row_shft_mtrx[k][j] << 1
+                elif fixed_matrix[i][k] == 0x03:
+                    raw_product = (row_shft_mtrx[k][j] << 1) ^ row_shft_mtrx[k][j]
+                else:
+                    raw_product = row_shft_mtrx[k][j]
+                # check if raw product out of GF(2^8) and mod '1 0001 1011' if so
+                if raw_product > 255:
+                    product = (raw_product ^ 0b100011011) & 0x0ff
                 else:
                     product = raw_product
-                mix_col_matrx[i][j] = mix_col_matrx[i][j] ^ product
+                mixed_entry ^= product
+            if mixed_entry > 255:
+                mix_col_matrx[i][j] = (mixed_entry ^ 0b100011011) & 0x0ff
+            else:
+                mix_col_matrx[i][j] = mixed_entry
 
     return np.asarray(mix_col_matrx)
 
+
+def arrange_key_into_columns(key_array):
+    column_arranged_key = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    key_array_element_index = 0
+    for i in range(4):
+        for j in range(4):
+            column_arranged_key[i][j] = key_array[key_array_element_index]
+            key_array_element_index += 1
+
+    return column_arranged_key
+
+
+def generate_all_key_columns(orig_key_column_form, s_box):
+    master_key_columns = []
+    # add first 4 columns to master key
+    for col in orig_key_column_form:
+        master_key_columns.append(col)
+
+    # generate column numbers 4-40
+    for col_num in range(4, 44):
+        # do this if column number is NOT multiple of 4
+        if col_num % 4 != 0:
+            new_col = [0, 0, 0, 0]
+            for i in range(4):
+                new_col[i] = master_key_columns[col_num - 1][i] ^ master_key_columns[col_num - 4][i]
+            master_key_columns.append(new_col)
+        else:
+            # this will be the resulting new column
+            new_col = [0, 0, 0, 0]
+
+            # transform W(col_num - 1) by shifting cyclically up by 1 (-1)
+            transformed_col_minus_1 = np.roll(master_key_columns[col_num - 1], -1)
+            # replace transformed column minus 1 with s-box values
+            s_box_subed = [0, 0, 0, 0]
+            s_box_subed_element_counter = 0
+            for element in transformed_col_minus_1:
+                high_bit_value = element >> 4
+                low_bit_value = element & 0x0f
+                s_box_subed[s_box_subed_element_counter] = s_box[high_bit_value][low_bit_value]
+                s_box_subed_element_counter += 1
+            # generate special ith special byte
+            special_byte = generate_special_byte(col_num)
+            # add special byte to first element in s-box subed array
+            top_byte = (s_box_subed[0]) ^ int(special_byte)
+            s_box_subed[0] = top_byte
+            transformed_col_minus_1 = s_box_subed
+            # generate new column - transformed W(i-1) ^ W(i-4)
+            for i in range(4):
+                new_col[i] = transformed_col_minus_1[i] ^ master_key_columns[col_num - 4][i]
+            master_key_columns.append(new_col)
+
+    return master_key_columns
+
+
+# when current column is a multiple of 4 when building master key, this method generates the special
+# bit to add to the top byte of the column
+def generate_special_byte(current_col_num):
+    raw_special_byte = 0b00000010 ** ((current_col_num - 4) / 4)
+    if raw_special_byte > 0xff:
+        special_byte = (int(raw_special_byte) ^ 0b100011011) & 0x0ff
+    else:
+        special_byte = raw_special_byte
+    return special_byte
+
+
+# transforms column arranged 4X4 matrix into row arranged 4X4 matrix
+def key_columns_to_rows(column_arng_key):
+    row_arranged_key = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for i in range(4):
+        for j in range(4):
+            row_arranged_key[j][i] = column_arng_key[i][j]
+    return row_arranged_key
 
 main()
 
